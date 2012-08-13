4001f83fc6505dc36ca9161799ace372b21e7503
Patrick Lerner <patricklerner@me.com>
2012-08-13 11:06:38 +0200

    Revert "Simplified showPiece"
    This reverts commit 3f2a2b1ba171d40ecc61aaa198465521c0f1bd41.

--- Chess.hs
+++ Chess.hs
@@ -6,7 +6,6 @@ import Test.HUnit
 
 import Data.Char
 import Control.Error.Util (note)
-import Data.Tuple (swap)
 
 type Board = [[Square]]
 
@@ -29,19 +28,37 @@ showBoard = unlines . (map . map) showSquare
 
 type Square = Maybe Piece
 
--- | Show a square using FEN notation or '.' for an empty square.
+-- | Show a square using FEN notation or ' ' for an empty square.
 showSquare :: Square -> Char
 showSquare = maybe '.' showPiece
 
--- | Read a square using FEN notation or '.' for an empty square.
+-- | Read a square using FEN notation or ' ' for an empty square.
 readSquare :: Char -> Either String Square
 readSquare '.' = return Nothing
 readSquare  c  = note errormsg $ fmap return (readPiece c)
   where errormsg = "Error reading square: " ++ show c ++ " is not a valid square"
 
 data Piece = Piece PColor PType deriving (Show)
-data PColor = White | Black deriving (Show, Eq)
-data PType = Pawn | Knight | Bishop | Rook | Queen | King deriving (Show, Eq)
+data PColor = White | Black deriving (Show)
+data PType = Pawn | Knight | Bishop | Rook | Queen | King deriving (Show)
+
+-- | Shows a piece using FEN notation.
+--
+-- * White pieces are "PNBRQG"
+-- * Black pieces are "pnbrqg"
+showPiece :: Piece -> Char
+showPiece (Piece White Pawn)   = 'P'
+showPiece (Piece White Knight) = 'N'
+showPiece (Piece White Bishop) = 'B'
+showPiece (Piece White Rook)   = 'R'
+showPiece (Piece White Queen)  = 'Q'
+showPiece (Piece White King)   = 'K'
+showPiece (Piece Black Pawn)   = 'p'
+showPiece (Piece Black Knight) = 'n'
+showPiece (Piece Black Bishop) = 'b'
+showPiece (Piece Black Rook)   = 'r'
+showPiece (Piece Black Queen)  = 'q'
+showPiece (Piece Black King)   = 'k'
 
 typeList :: [(Char, PType)]
 typeList =  [('p', Pawn)
@@ -51,16 +68,6 @@ typeList =  [('p', Pawn)
             ,('q', Queen)
             ,('k', King)
             ]
-
--- | Shows a piece using FEN notation.
---
--- * White pieces are "PNBRQG"
--- * Black pieces are "pnbrqg"
-showPiece :: Piece -> Maybe Char
-showPiece (Piece color ptype) = parseColor lookupChar
-  where lookupChar          = lookup ptype (map swap typeList)
-        parseColor Nothing  = '.'
-        parseColor (Just c) = if color == White then toUpper c else c
 
 -- | Reads a piece using FEN notation.
 --
